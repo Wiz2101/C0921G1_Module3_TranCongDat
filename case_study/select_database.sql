9751df8b193711babdec88771ceb7e359b98ffd6
Wiz <trancongdat.2101@gmail.com>
2021-12-12 18:07:08 +0700

    update casestudy

--- case_study/select_database.sql
+++ case_study/select_database.sql
@@ -66,4 +66,94 @@ FROM
         LEFT JOIN
     dich_vu_di_kem ON dich_vu_di_kem.ma_dich_vu_di_kem = hop_dong_chi_tiet.ma_dich_vu_di_kem
 GROUP BY ma_hop_dong
-ORDER BY ma_khach_hang ASC
+ORDER BY ma_khach_hang ASC, ma_hop_dong DESC;
+
+-- Câu 6:
+-- Hiển thị ma_dich_vu, ten_dich_vu, dien_tich, chi_phi_thue, ten_loai_dich_vu của tất cả các loại dịch vụ chưa từng được khách hàng 
+-- thực hiện đặt từ quý 1 của năm 2021 (Quý 1 là tháng 1, 2, 3).
+
+SELECT 
+    dich_vu.ma_dich_vu,
+    dich_vu.ten_dich_vu,
+    dich_vu.dien_tich,
+    dich_vu.chi_phi_thue,
+    loai_dich_vu.ten_loai_dich_vu
+FROM
+    dich_vu
+        JOIN
+    loai_dich_vu ON dich_vu.ma_loai_dich_vu = loai_dich_vu.ma_loai_dich_vu
+        JOIN
+    hop_dong ON dich_vu.ma_dich_vu = hop_dong.ma_dich_vu
+WHERE
+    dich_vu.ma_dich_vu NOT IN (SELECT 
+            hop_dong.ma_dich_vu
+        FROM
+            hop_dong
+        WHERE
+            MONTH(hop_dong.ngay_lam_hop_dong) BETWEEN 1 AND 3)
+GROUP BY ten_dich_vu;
+
+-- Câu 7:
+-- Hiển thị thông tin ma_dich_vu, ten_dich_vu, dien_tich, so_nguoi_toi_da, chi_phi_thue, ten_loai_dich_vu của tất cả các loại dịch vụ đã từng được khách hàng 
+-- đặt phòng trong năm 2020 nhưng chưa từng được khách hàng đặt phòng trong năm 2021.
+
+SELECT 
+    dich_vu.ma_dich_vu,
+    dich_vu.ten_dich_vu,
+    dich_vu.dien_tich,
+    dich_vu.so_nguoi_toi_da,
+    dich_vu.chi_phi_thue,
+    loai_dich_vu.ten_loai_dich_vu
+FROM
+    dich_vu
+        JOIN
+    loai_dich_vu ON dich_vu.ma_loai_dich_vu = loai_dich_vu.ma_loai_dich_vu
+        JOIN
+    hop_dong ON dich_vu.ma_dich_vu = hop_dong.ma_dich_vu
+WHERE
+    hop_dong.ma_dich_vu NOT IN (SELECT 
+            hop_dong.ma_dich_vu
+        FROM
+            hop_dong
+        WHERE
+            YEAR(hop_dong.ngay_lam_hop_dong) = 2021)
+GROUP BY ma_dich_vu;
+
+-- Câu 8:
+-- Hiển thị thông tin ho_ten khách hàng có trong hệ thống, với yêu cầu ho_ten không trùng nhau.
+
+SELECT 
+    khach_hang.ho_ten
+FROM
+    khach_hang
+HAVING COUNT(khach_hang.ho_ten) > 1;
+
+-- Câu 9:
+-- Thực hiện thống kê doanh thu theo tháng, nghĩa là tương ứng với mỗi tháng trong năm 2021 thì sẽ có bao nhiêu khách hàng thực hiện đặt phòng.
+
+SELECT 
+    MONTH(hop_dong.ngay_lam_hop_dong) as 'thang',
+    COUNT(MONTH(hop_dong.ngay_lam_hop_dong)) as 'so_luong_khach_hang'
+FROM
+    hop_dong
+WHERE
+    YEAR(hop_dong.ngay_lam_hop_dong) = 2021
+GROUP BY MONTH(hop_dong.ngay_lam_hop_dong)
+ORDER BY MONTH(hop_dong.ngay_lam_hop_dong) ASC;
+
+-- Câu 10:
+-- Hiển thị thông tin tương ứng với từng hợp đồng thì đã sử dụng bao nhiêu dịch vụ đi kèm. 
+-- Kết quả hiển thị bao gồm ma_hop_dong, ngay_lam_hop_dong, ngay_ket_thuc, tien_dat_coc, so_luong_dich_vu_di_kem (được tính dựa trên việc sum so_luong ở dich_vu_di_kem).
+
+SELECT 
+    hop_dong.ma_hop_dong,
+    hop_dong.ngay_lam_hop_dong,
+    hop_dong.ngay_ket_thuc,
+    IFNULL(hop_dong.tien_dat_coc, 0),
+    SUM(IFNULL(hop_dong_chi_tiet.so_luong, 0))
+FROM
+    hop_dong
+        LEFT JOIN
+    hop_dong_chi_tiet ON hop_dong.ma_hop_dong = hop_dong_chi_tiet.ma_hop_dong
+GROUP BY ma_hop_dong
+ORDER BY ma_hop_dong ASC;
